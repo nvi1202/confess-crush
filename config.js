174a9e14e22c48ce4a1d20a96f0d488b41b3a473
nvi1202 <nvi12022007@gmail.com>
2025-02-10 17:46:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    titleWeb: "Yearbook Invitation",
+    introTitle: 'Bạn yêu',
+    introDesc: 'Đây là ngày mà tớ muốn lưu lại những khoảnh khắc đẹp đẽ nhất của thời học sinh.Hy vọng những bức ảnh thanh xuân tươi đẹp của tớ sẽ có sự tham gia của cậu.',
+    btnIntro: 'NHỚ ĐẾN NHA :3',
+    title: 'SỰ CÓ MẶT CỦA CẬU CHÍNH LÀ NIỀM VUI CỦA Nvi! 🥰',
+    desc: '8:00 gặp nhau ở trường nhé!',
+    btnYes: 'MÌNH SẼ ĐẾN :3',
+    btnNo: 'Không nha :)',
+    question: 'Mình rất mong chờ sự góp mặt của cậu',
+    btnReply: 'Gửi cho Nvi',
+    reply: 'yêuuu',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'NHỚ ĐẾN ĐÚNG GIỜ NHAAA.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
