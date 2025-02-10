d4dafb8a0072524f76682bcf145959098118ca44
nvi1202 <nvi12022007@gmail.com>
2025-02-10 20:52:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "JDJTFH",
-    introTitle: 'Tên người yêu bạn',
+    introTitle: 'TêKUHU',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
